e771aaf79373e9fd2b3d507104724b157bf97dc2
mmayankvats-max <mmayankvats@gmail.com>
2026-07-28 07:07:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/service-worker.js")
+def service_worker():
+    return send_from_directory(
+        app.root_path,
+        "service-worker.js",
+        mimetype="application/javascript"
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
